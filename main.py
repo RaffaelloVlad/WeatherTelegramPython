93bc22ce4ace3fe1b39e5e74285e2747aa862493
RaffaelloVlad <vladsav37@gmail.com>
2023-07-16 22:49:30 +0200

    fix log
    when nicknames had unicode characters the sweat was dying

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 from apikey import *
 from dictionaries import *
 from aiogram.dispatcher import FSMContext
+import datetime
 
 
 # Установка уровня логирования
@@ -15,7 +16,7 @@ bot = Bot(token=f'{TelebotAPIKey}')
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-output_file = 'E://BotPyWeather//WeatherTelegramPython//.git//sourse//output.txt'
+output_file = 'E://BotPyWeather//WeatherTelegramPython//output.txt'
 
 # Обработчик команды /start
 @dp.message_handler(commands=['start'])
@@ -49,70 +50,12 @@ def kelToCel(fah):
     cel = fah - 273.15
     return round(cel)
 
-def map_weather_type(weather_type):
-    weather_mapping = {
-        'clear sky': 'Чисте небо',
-        'few clouds': 'Небагато хмар',
-        'overcast clouds': 'хмарно затягнуте небо',
-        'scattered clouds': 'Розсіяні хмари',
-        'broken clouds': 'Розірвані хмари',
-        'shower rain': 'Злива',
-        'rain': 'Дощ',
-        'light rain': 'невеликий дощ',
-        'moderate rain': 'помірний дощ',
-        'heavy intensity rain': 'сильний дощ',
-        'very heavy rain': 'дуже сильний дощ',
-        'extreme rain': 'сильний дощ',
-        'freezing rain': 'крижаний дощ',
-        'light intensity shower rain': 'дощ легкої інтенсивності',
-        'heavy intensity shower rain': 'сильний зливовий дощ',
-        'ragged shower rain': 'рваний душовий дощ',
-        'snow': 'Сніг',
-        'light snow': 'невеликий сніг',
-        'heavy snow': 'сильний сніг',
-        'sleet': 'мокрий сніг',
-        'light shower sleet': 'легкий дощ зі снігом',
-        'shower sleet': 'злава мокрий сніг',
-        'light rain and snow': 'невеликий дощ зі снігом',
-        'rain and snow': 'дощ і сніг',
-        'light shower snow': 'дрібний сніг',
-        'shower snow': 'снігопад',
-        'heavy shower snow': 'сильний снігопад',
-        'thunderstorm': 'Гроза',
-        'thunderstorm with light rain': 'гроза з невеликим дощем',
-        'thunderstorm with rain': 'гроза з дощем',
-        'thunderstorm with heavy rain': 'гроза з сильним дощем',
-        'light thunderstorm': 'легка гроза',
-        'heavy thunderstorm': 'сильна гроза',
-        'ragged thunderstorm': 'обірвана гроза',
-        'thunderstorm with light drizzle': 'гроза з легким дощем',
-        'thunderstorm with drizzle': 'гроза з дощем',
-        'thunderstorm with heavy drizzle': 'гроза з сильним дощем',
-        'mist': 'Туман',
-        'smoke': 'дим',
-        'haze': 'серпанок',
-        'sand/dust whirls': 'завихрення піску/пилу',
-        'fog': 'туман',
-        'sand': 'пісок',
-        'dust': 'пил',
-        'volcanic ash': 'вулканічний попіл',
-        'squalls': 'шквали',
-        'tornado': 'торнадо',
-        'light intensity drizzle': 'інтенсивність світла мряка',
-        'drizzle': 'мряка',
-        'heavy intensity drizzle': 'сильний дощ',
-        'light intensity drizzle rain': 'мрячний дощ слабкої інтенсивності',
-        'drizzle rain': 'мрячить дощ',
-        'heavy intensity drizzle rain': 'сильна мряка',
-        'shower rain and drizzle': 'мряка і дрібний дощ',
-        'heavy shower rain and drizzle': 'сильний зливовий дощ та мряка',
-        'shower drizzle': 'дощова мряка',
-    }
-    return weather_mapping.get(weather_type, 'Неизвестно')
-
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
 async def handle_text(message: types.Message, state: FSMContext):
     
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
     state_data = await state.get_data()
     language = state_data.get('language')
     if not language:    # Если значение language не установлено
@@ -133,16 +76,16 @@ async def handle_text(message: types.Message, state: FSMContext):
     res = requests.get(url)
     data = res.json()
     
-    # user_id = message.from_user.id
-    # user_first_name = message.from_user.first_name
-    # user_last_name = message.from_user.last_name
-    # user_username = message.from_user.username
+    user_id = message.from_user.id
+    user_first_name = message.from_user.first_name
+    user_last_name = message.from_user.last_name
+    user_username = message.from_user.username
 
     if data['count'] == 0:
-        # with open(output_file, 'a') as file:
-            # file.write(f"FAILD_MESSAGE: User ID: {user_id}, First Name: {user_first_name}, Last Name: {user_last_name}, Username: {user_username},text: {input_message}\n")
+        with open(output_file, 'a', encoding="utf-8") as file:
+            file.write(f"[{formatted_datetime}] FAILD_MESSAGE: User ID: {user_id}, First Name: {user_first_name}, Last Name: {user_last_name}, Username: {user_username},text: {input_message}\n")
         await message.reply("Місто не знайдено. Будь ласка, введіть правильну назву міста.")
-        #return
+        return
 
     weatherInfo['Temperature'] = kelToCel(data['list'][0]['main']['temp'])
     weatherInfo['weather type'] = data['list'][0]['weather'][0]['description']
@@ -158,8 +101,8 @@ async def handle_text(message: types.Message, state: FSMContext):
     #result_string = f"Вы ввели: {input_message}"#test
 
 
-    # with open(output_file, 'a') as file:
-    #     file.write(f"User ID: {user_id}, First Name: {user_first_name}, Last Name: {user_last_name}, Username: {user_username},text: {input_message}\n")
+    with open(output_file, 'a', encoding="utf-8") as file:
+        file.write(f"[{formatted_datetime}] User ID: {user_id}, First Name: {user_first_name}, Last Name: {user_last_name}, Username: {user_username}, Text: {input_message}\n")
 
     await message.reply(result_string)
 
